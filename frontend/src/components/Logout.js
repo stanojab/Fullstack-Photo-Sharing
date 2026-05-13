67a8aeaf67704f7811cadf4cf70c6803f1602e46
stanojab <stanojabozinov0@gmail.com>
2026-05-13 10:22:10 +0200

    fix(Logout): add credentials: include so session is actually destroyed on server

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -1,21 +1,21 @@
-import { useEffect, useContext } from 'react';
-import { UserContext } from '../userContext';
-import { Navigate } from 'react-router-dom';
+import { useEffect, useContext } from "react";
+import { UserContext } from "../userContext";
+import { Navigate } from "react-router-dom";
 
-function Logout(){
-    const userContext = useContext(UserContext);
-     
-    useEffect(function(){
-        const logout = async function(){
-            userContext.setUserContext(null);
-            const res = await fetch("http://localhost:3001/users/logout");
-        }
-        logout();
-    }, []);
+function Logout() {
+  const userContext = useContext(UserContext);
 
-    return (
-        <Navigate replace to="/" />
-    );
+  useEffect(() => {
+    const logout = async () => {
+      userContext.setUserContext(null);
+      await fetch("http://localhost:3001/users/logout", {
+        credentials: "include",
+      });
+    };
+    logout();
+  }, []);
+
+  return <Navigate replace to="/" />;
 }
 
 export default Logout;
